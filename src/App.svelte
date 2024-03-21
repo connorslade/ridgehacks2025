297858b6b89867976ec61aeda72eb39d4960b61a
Connor Slade <connor@connorcode.com>
2024-03-21 15:46:27 -0400

    Add college students

--- src/App.svelte
+++ src/App.svelte
@@ -46,7 +46,7 @@
         the <Link href="https://ridgecompsci.club"
           >Ridge Computer Science Club</Link
         >. Following a single day, 12-hour hacking schedule, Ridgehacks offers a
-        free hackathon for Middle and High School Students from <Time
+        free hackathon for Middle, High School, and College Students from <Time
           hour={8}
         /> until <Time hour={22} />.
       </p>
