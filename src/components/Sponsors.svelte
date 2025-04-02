8d7fe613f7f376ef3d666e2375f6153acb32c745
Connor Slade <connor@connorcode.com>
2025-04-02 17:28:38 -0400

    Add sponsors

--- src/components/Sponsors.svelte
+++ src/components/Sponsors.svelte
@@ -10,21 +10,74 @@
         >.
     </p>
 
-    <div class="sponsors"></div>
+    <div class="sponsors">
+        <Sponsor
+            name="Balsamiq"
+            image="balsamiq.png"
+            link="https://balsamiq.com"
+        />
+        <Sponsor
+            name="Buona Pizza"
+            image="buona-pizza.svg"
+            link="https://www.buonapizzanj.com"
+        />
+        <Sponsor name="CoCalc" image="cocalc.png" link="https://cocalc.com" />
+        <Sponsor name="Gar" image="gar.png" />
+        <Sponsor
+            name="Hack United"
+            image="hackunited.png"
+            link="https://hackunited.org"
+        />
+        <Sponsor
+            name="Hoke Capital"
+            image="hoke-capital.png"
+            link="https://www.hokecapital.com"
+        />
+        <Sponsor
+            name="Incogni"
+            image="incogni.png"
+            link="https://incogni.com"
+        />
+        <Sponsor
+            name="Nord Pass"
+            image="nord-pass.png"
+            link="https://nordpass.com"
+        />
+        <Sponsor
+            name="Nord VPN"
+            image="nord-vpn.png"
+            link="https://nordvpn.com"
+        />
+        <Sponsor
+            name="Perrottis"
+            image="perrottis.svg"
+            link="https://www.perrottis.com"
+        />
+        <Sponsor
+            name="Bernards Township Police Department"
+            image="police-department.png"
+            link="https://www.bernards.org/departments/police"
+        />
+        <Sponsor
+            name="Ridge Marketing"
+            image="ridge-marketing.png"
+            link="https://ridgemarketing.com"
+        />
+        <Sponsor name="Saily" image="saily.png" link="https://saily.com" />
+        <Sponsor
+            name="Verizon"
+            image="verizon.png"
+            link="https://www.verizon.com"
+        />
+    </div>
 </Section>
 
 <style lang="scss">
     .sponsors {
         display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+        grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
         gap: 24px;
         justify-items: center;
         padding-bottom: 10px;
-    }
-
-    @media (max-width: 583px) {
-        .sponsors {
-            grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
-        }
     }
 </style>
